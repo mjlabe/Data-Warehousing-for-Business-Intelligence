b2e378bb685356ad5f66bdd6f06ae3f2e2d2aafa
mjlabe <mjlabe@gmail.com>
2022-01-26 19:41:53 -0500

    compare with solutions

--- database-management-essentials/module4/solutions.sql
+++ database-management-essentials/module4/solutions.sql
@@ -5,36 +5,38 @@ FROM intercollegiate_athletic_database.Customer;
 #2
 SELECT CustNo, CustName, Phone, City, State
 FROM intercollegiate_athletic_database.Customer
-WHERE State="CO";
+WHERE State = "CO";
 
 #3
 SELECT *
 FROM EventRequest
 WHERE EstCost > 4000
+ORDER BY DateHeld;
 
 #4
 SELECT EventNo, DateHeld, EstAudience, Status
 FROM EventRequest
-WHERE EstAudience > 9000
-OR (Status="pending"
-AND EstAudience > 7000);
+WHERE (EstAudience > 9000 AND Status = "approved")
+   OR (Status = "pending" AND EstAudience > 7000);
 
 #5
 SELECT EventNo, DateHeld, Customer.CustNo, CustName, City
 FROM EventRequest
-INNER JOIN Customer on EventRequest.CustNo = Customer.CustNo
-WHERE Customer.City="Boulder"
+         INNER JOIN Customer on EventRequest.CustNo = Customer.CustNo
+WHERE Customer.City = "Boulder"
+  AND DateHeld BETWEEN '2018-12-01' AND '2018-12-31';
 
 #6
-SELECT PlanNo, AVG(Cast(NumberFld as DECIMAL)) as AvgNumberFld
+SELECT PlanNo, AVG(NumberFld) as AvgNumberFld
 FROM EventPlanLine
-WHERE LocNo="L100"
-GROUP BY PlanNo
+WHERE LocNo = "L100"
+GROUP BY PlanNo;
 
 #7
 SELECT PlanNo,
-       AVG(Cast(NumberFld as DECIMAL)) as AvgNumberFld
+       AVG(NumberFld) as AvgNumberFld,
+       COUNT(*) AS NoLines
 FROM EventPlanLine
-WHERE LocNo="L100"
-AND LineNo>=2
+WHERE LocNo = "L100"
 GROUP BY PlanNo
+HAVING COUNT(*)>1
